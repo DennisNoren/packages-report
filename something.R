fa0376f49c988151588933d22c1c41b16ab53999
Dennis Noren <dennisdnoren@gmail.com>
2020-01-27 16:20:31 -0800

    a simple report

--- something.R
+++ something.R
@@ -1,13 +1,13 @@
 # Separate, flatten, and trim values in the vector
 clean <- function(vec) {
-    +   values <- strsplit(vec, ",")
-    +   flat_values <- unlist(values)
-    +   trimmed_values <- str_trim(flat_values)
-    +   trimmed_values
-    + }
+       values <- strsplit(vec, ",")
+       flat_values <- unlist(values)
+       trimmed_values <- str_trim(flat_values)
+       trimmed_values
+     }
 # Read data and get unique climate values
   get_climates <- function() {
-      +   planets <- readr::read_csv2(here::here("activities/planets.csv"))
-      +   unique_climate <- uniquify(planets$climate)
-      +   unique_climate
-      + }
+         planets <- readr::read_csv2(here::here("activities/planets.csv"))
+         unique_climate <- uniquify(planets$climate)
+         unique_climate
+       }
